9a821fb41a64683d3d3ef5d75f74140c69ac9f33
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-19 18:55:53 -0300

    aula 6 criação de todas as rotas

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -5,5 +5,20 @@ const NoteService = require('../services/NoteService');
 module.exports = {
     ping:(req, res)=> {
         res.json({pong:true})
-    }
+    },
+    all:() =>{
+
+    },
+    one:() =>{
+
+    },
+    new:() =>{
+
+    },
+    edit:() =>{
+
+    },
+    delete:() =>{
+
+    },
 }
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,15 @@ const router = express.Router();
 const NoteController = require('./controllers/NoteController')
 
 router.get('/ping', NoteController.ping);
+
+
+router.get('/notes', NoteController.all)
+router.get('/notes/:id', NoteController.one)
+router.post('/notes', NoteController.new)
+router.put('/notes/:id', NoteController.edit)
+router.delete('/notes/:id', NoteController.delete)
+
+
+
+
 module.exports = router;
